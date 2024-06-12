bea23850609c746332cea458924a48267873f8c6
Wesley Lau <wesleylau.wcl@gmail.com>
2024-06-12 03:27:58 -0400

    worked morn on exporting data

--- analysis.py
+++ analysis.py
@@ -5,5 +5,22 @@ from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.library.parameters import Season
 from nba_api.stats.library.parameters import SeasonType
+from nba_api.stats.library.parameters import SeasonTypePlayoffs
 
 import json
+
+nba_teams = teams.get_teams()
+# Select the dictionary for the Celtics, which contains their team ID
+celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
+celtics_id = celtics['id']
+
+
+print(celtics_id)
+# Query for games where the Celtics were playing
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
+# The first DataFrame of those returned is what we want.
+games = gamefinder.get_data_frames()[0]
+# print(games.head())
+
+total_gmae = games.groupby(games.SEASON_ID.str[-4:])[['GAME_ID']].count().loc['2015':]
+# print(total_gmae)
